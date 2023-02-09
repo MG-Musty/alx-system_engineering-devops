64016928bc1e203d27b68f6a3753707f911a46f2
MG-Musty <mustaphaaliyugaladima@gmail.com>
2023-02-09 10:25:42 -0800

    export to Json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,30 @@
-!/usr/bin/python3
-"""Returns tODO list information for a given ID."""
+#!/usr/bin/python3
+""" Script that uses JSONPlaceholder API to get information about employee """
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    user_name = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    user_id = sys.argv[1]
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": user_name
-            } for t in todos]}, jsonfile)
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
+
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        dict_task = {"task": task.get('title'),
+                     "completed": task.get('completed'),
+                     "username": name}
+        l_task.append(dict_task)
+
+    d_task = {str(userid): l_task}
+    filename = '{}.json'.format(userid)
+    with open(filename, mode='w') as f:
+        json.dump(d_task, f)
